d147cafcd4ebf0861245c89e74825913949c9f36
Matt DeKrey <matt.dekrey@principlestudios.com>
2021-12-17 09:32:20 -0600

    Initial loading of a project

---
+++ PrincipleStudios.CodeFixes/Program.cs
@@ -0,0 +1,41 @@
+﻿// Automatically runs Roslyn analyzers on a project
+// Based on code from:
+// - https://github.com/Vannevelj/RoslynTester/blob/master/RoslynTester/RoslynTester/Helpers/CodeFixVerifier.cs#L109
+// - https://github.com/kzu/AutoCodeFix
+
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Build.Locator;
+
+var services = new ServiceCollection();
+services.AddLogging(logging => logging.AddConsole());
+var provider = services.BuildServiceProvider();
+
+MSBuildLocator.RegisterDefaults();
+
+var cli = new CommandLineApplication(true);
+cli.Description = GetVersionInfo();
+cli.HelpOption("-? | -h | --help");
+cli.Option("-p | --project", "Path to the project file(s)", CommandOptionType.MultipleValue);
+cli.Option("-n | --dry-run", "Log changes, but do not apply", CommandOptionType.NoValue);
+cli.OnExecute(async () =>
+{
+    if (cli.Arguments.Any(arg => arg.Value == null))
+    {
+        cli.ShowHelp();
+        return 1;
+    }
+
+    var projects = cli.Options.FindAll(opt => opt.LongName == "project").Select(opt => opt.Value());
+    using var workspace = await ActivatorUtilities.GetServiceOrCreateInstance<WorkspaceBuilder>(provider).BuildWorkspace(projects: projects);
+
+    return 0;
+});
+return cli.Execute(args);
+
+
+static string GetVersionInfo()
+{
+    return $"{typeof(Program).Namespace} v{typeof(Program).Assembly.GetName().Version}";
+}
---
+++ PrincipleStudios.CodeFixes/WorkspaceBuilder.cs
@@ -0,0 +1,34 @@
+﻿// Automatically runs Roslyn analyzers on a project
+// Based on code from:
+// - https://github.com/Vannevelj/RoslynTester/blob/master/RoslynTester/RoslynTester/Helpers/CodeFixVerifier.cs#L109
+// - https://github.com/kzu/AutoCodeFix
+
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.Extensions.Logging;
+
+class WorkspaceBuilder
+{
+    private readonly ILogger<WorkspaceBuilder> logger;
+
+    public WorkspaceBuilder(ILogger<WorkspaceBuilder> logger)
+    {
+        this.logger = logger;
+    }
+
+    public async Task<MSBuildWorkspace> BuildWorkspace(IEnumerable<string> projects)
+    {
+        var workspace = MSBuildWorkspace.Create();
+        workspace.WorkspaceFailed += (sender, args) => logger.LogError($"{args.Diagnostic.Kind}: {args.Diagnostic.Message}");
+        try
+        {
+            await Task.WhenAll(projects.Select(project => workspace.OpenProjectAsync(project)));
+    
+            return workspace;
+        }
+        catch
+        {
+            workspace?.Dispose();
+            throw;
+        }
+    }
+}
